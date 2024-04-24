02dfc0a2867ec92e19b8475ff02015a8892a9235
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-04-24 00:37:28 +0000

    Update Navbar.jsx

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -11,17 +11,17 @@ const Navbar = () => {
   return (
     <nav className="pt-2">
       <div className="flex justify-between items-center">
-        <h1 className="font-bold text-2xl text-orange-500">List</h1>
+        <h1 className="font-bold text-2xl ml-2 text-orange-500">List</h1>
         <div className="md:flex md:w-1/2 search-div">
-          <svg className="absolute h-5 w-6 text-gray-400 hover:text-gray-500 s-icon" xmlns="http://www.w3.org/2000/svg" fill="none"
+          <svg className="absolute h-5 w-6 text-gray-400 -ml-4 hover:text-gray-500 s-icon" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
           </svg>
-          <input type="text" placeholder="Find (almost) anything" className="border border-gray-200 rounded-2xl py-1 px-2 pl-10 w-96 outline-none search" />
+          <input type="text" placeholder="Find (almost) anything" className="border -ml-6 border-gray-200 rounded-2xl py-1 px-2 pl-10 w-96 outline-none search" />
         </div>
-        <div className="md:hidden ">
-          <button onClick={toggleMenu} className="">
+        <div className="md:hidden -mr-6">
+          <button onClick={toggleMenu} >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6 h-icon"
@@ -29,11 +29,12 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {isOpen ? (
+              {isOpen ?  (
                 <path
+                  className='X'
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth={2}
+                  strokeWidth={2.5}
                   d="M6 18L18 6M6 6l12 12"
                 />
               ) : (
@@ -47,8 +48,8 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
-        <div className={`md:flex ${isOpen ? 'block' : 'hidden'} nav-links`}>
-          <ul className="md:flex items-center space-x-4 ">
+        <div className={`md:grid ${isOpen ? 'block' : 'hidden'} nav-links`}>
+          <ul className="md:grid items-center space-x-4 nav ">
             <li className='nav-item'>
               <a href="#" className="text-blue-700  hover:text-gray-300">
                 Item 1
@@ -59,7 +60,7 @@ const Navbar = () => {
                 Item 2
               </a>
             </li>
-            <li>
+            <li className='btn_logIn'>
               <button className="text-gray-900 rounded-3xl bg-gray-100 py-1 px-4 login">
                 Login or Sign up
               </button>
